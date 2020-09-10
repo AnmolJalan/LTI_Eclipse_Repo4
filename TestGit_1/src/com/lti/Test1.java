a2400a6d008247e536d110d0ffaf00503719a8c8
Anmol Jalan <Anmol Jalan>
2020-09-10 15:16:33 +0530

    second file

--- TestGit_1/src/com/lti/Test1.java
+++ TestGit_1/src/com/lti/Test1.java
@@ -1,10 +1,10 @@
 package com.lti;
 
-public class Test {
+public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test..,,");
+	  System.out.println("test");
 
 	}
 
